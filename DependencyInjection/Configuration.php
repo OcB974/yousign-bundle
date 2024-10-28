1f04c9f88ea8646d1902ddceccf3e53aa84c64a9
OcB974 <thomas.techer.f@gmail.com>
2024-10-28 13:21:12 +0400

    Fix ConfigTreeBuilder Declaration

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -6,16 +6,13 @@ use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
 /**
- * This is the class that validates and merges configuration from your app/config files
+ * This is the class that validates and merges configuration from your app/config files.
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html#cookbook-bundles-extension-config-class}
  */
 class Configuration implements ConfigurationInterface
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function getConfigTreeBuilder()
+    public function getConfigTreeBuilder(): TreeBuilder
     {
         $treeBuilder = new TreeBuilder('neyric_yousign');
         $rootNode = $treeBuilder->getRootNode();
